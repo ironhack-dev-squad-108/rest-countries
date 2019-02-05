98343c812aa8a5447766447080bbd0f29ab2520e
Maxence Bouret <maxence.bouret@gmail.com>
2019-02-05 15:21:42 +0100

    Update

--- script.js
+++ script.js
@@ -6,28 +6,41 @@ const stockTicket = "amzn";
 
 stockInfo.get(`${stockTicket}/chart`)
   .then(response => {
-    printTheChart(response.data);
+    printTheChart(response.data, stockTicket);
   })
   .catch(error => {
     console.log(error);
   });
 
-function printTheChart(stockData){
+let datasets = []
+
+function printTheChart(stockData, stockTicket) {
   const stockLabels = stockData.map(element => element.date);
   const stockPrice = stockData.map(element => element.close);
-	console.log('TCL: printTheChart -> stockLabels', stockLabels)
-	console.log('TCL: printTheChart -> stockPrice', stockPrice)
+  datasets.push({
+    label: stockTicket,
+    backgroundColor: 'rgba(255, 99, 132, 0)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: stockPrice, // [1629.51, 1656.58, ...]
+    // data: stockPrice.map(val => val/stockPrice[0]), // easier solution to compare
+  })
   const ctx = document.getElementById('myChart').getContext('2d');
   const chart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: stockLabels, // ["2019-01-07", "2019-01-08", ...]
-      datasets: [{
-        label: "Stock Chart",
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: stockPrice, // [1629.51, 1656.58, ...]
-      }]
+      datasets: datasets
     }
   });
 };
+
+document.getElementById('update-1').onclick = function () {
+  let stockTicket1 = document.getElementById('stock-ticket-1').value
+  stockInfo.get(`${stockTicket1}/chart`)
+    .then(response => {
+      printTheChart(response.data, stockTicket1);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
