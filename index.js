6ca2a12bd0b2990601a1e9400f74679c52d5a02b
Maxence Bouret <maxence.bouret@gmail.com>
2019-02-05 14:06:25 +0100

    Update

---
+++ index.js
@@ -0,0 +1,18 @@
+const restCountriesApi = axios.create({
+  baseURL: 'https://restcountries.eu/rest/v2/name/'
+});
+
+function getCountryInfo(theName) {
+  restCountriesApi.get(theName)
+  .then(responseFromAPI => {
+      console.log('Response from API is: ', responseFromAPI.data);           
+})
+.catch(err => {
+  console.log('Error is: ', err);
+  })
+}
+
+document.getElementById("theButton").onclick = function(){
+  const country = document.getElementById("theInput").value;       
+  getCountryInfo(country);
+}
